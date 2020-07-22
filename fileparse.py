8398e2018e9f059319e51a2bc3926f4cfd898e3d
norona <sabirm@mun.ca>
2020-07-21 23:24:26 -0230

    exercise 3.7

--- fileparse.py
+++ fileparse.py
@@ -1,12 +1,13 @@
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers = True):
+def parse_csv(filename, select=None, types=None, has_headers = True, delimiter=','):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
+        
         # Read the file headers
         if has_headers:
             headers = next(rows)
@@ -43,4 +44,7 @@ print(records)
 records= parse_csv('Data/portfolio.csv', types= [str, int], select = ['name', 'shares'])
 print(records)
 records= parse_csv('Data/prices.csv',types = [str, float], has_headers=False)
+print(records)
+
+records= parse_csv('Data/portfolio.dat',types = [str,int, float], delimiter= ' ')
 print(records)
